52be448c8cd415b316c426a915f945be28ad75f0
Sung Hyun Jang <hoko7022@gmail.com>
2024-08-01 11:20:21 +0900

    논리 연산자 활용하기, sort 활용하기, find 활용하기 두가지

--- 240730.js
+++ 240730.js
@@ -71,3 +71,23 @@ const icedOnly = starbucks.filter((drink) => {
 })
 
 console.log(icedOnly);
+
+
+function sortArray(arr) {
+  // 여기에 sort를 사용하여 작성하세요
+  const result = arr.sort(function (인자1, 인자2) {
+    return 인자2 - 인자1;
+  });
+  return result;
+}
+function sortArray2(arr) {
+  // 여기에 sort를 사용하여 작성하세요
+  const result = arr.sort(function (인자1, 인자2) {
+    return 인자1.localeCompare(인자2);
+  });
+  return result;
+}
+
+// 예시 출력
+console.log(sortArray([3, 1, 4, 1, 5, 9])); // 출력: [1, 1, 3, 4, 5, 9]
+console.log(sortArray2(["바나나", "사과", "포도"])); // 출력: ['바나나', '사과', '포도']
